0f19d74da107f61fed6a9fcc59a07295ce9af17c
Vitor <v.e.silva3007@gmail.com>
2026-04-17 11:28:33 -0300

    Commit da aula 2

--- banco de dados/atividades.sql
+++ banco de dados/atividades.sql
@@ -43,3 +43,43 @@ alter table livros alter column titulo type varchar(200);
 
 --Adicionar regra
 alter table livros add constraint chk_preco check (preco > 0);
+
+-- Codigo inconsistente
+-- create table consultas (
+--   id integer,
+--   paciente varchar(100),
+--   medico varchar(100),
+--   data_consulta timestamp,
+--   valor real,
+--   status varchar(50)
+-- );
+
+--Codigo corrigido
+create table if not exists consultas (
+  id serial primary key,
+  paciente varchar(100) not null,
+  medico varchar(100) references medicos(id) not null on delete restrict,
+  data_consulta timestamptz default now()
+  valor numeric(1000,2) not null,
+  status varchar(50) default 'agendado'
+);
+
+--Codigo inconsistente 2
+-- create table consultas (
+--   id serial primary key,
+--   cliente_id integer references clientes(id),
+--   total double precision,
+--   desconto numeric(10,2),
+--   criado_em timestamp,
+--   status varchar(20) default 'pendente'
+-- );
+
+--Código corrigido 2
+create table if not exists pedidos (
+  id serial primary key,
+  cliente_id integer references clientes(id) not null on delete restrict,
+  total double precision,
+  desconto numeric(10,2) check (desconto >= 0 and desconto <= 100),
+  criado_em timestamptz default now(),
+  status varchar not null default 'pendente'
+);
