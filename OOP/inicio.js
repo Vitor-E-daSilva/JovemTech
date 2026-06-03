5044141dfc8deb3f99b67af037df5b45063fa881
Vitor <v.e.silva3007@gmail.com>
2026-06-03 11:15:54 -0300

    feat: continuacao dos estudos em oop

--- OOP/inicio.js
+++ OOP/inicio.js
@@ -32,18 +32,83 @@ console.log(carro.exibirInfo(), moto.exibirInfo(), total)
 
 
 
-// Exemplo de imperativo
-const alunos = [{aluno: "Maria", nota: 8}, {aluno: "Paulo", nota: 3}]
-let somaNotas = 0;
-let aprovados = [];
+// Exemplo de imperativo ↓
+// const alunos = [{aluno: "Maria", nota: 8}, {aluno: "Paulo", nota: 3}]
+// let somaNotas = 0;
+// let aprovados = [];
 
-for (let i = 0; i < alunos.length; i++) {
-    somaNotas += alunos[i].nota;
+// for (let i = 0; i < alunos.length; i++) {
+//     somaNotas += alunos[i].nota;
 
-    if (alunos[i].nota >= 7) {
-        aprovados.push(alunos[i])
+//     if (alunos[i].nota >= 7) {
+//         aprovados.push(alunos[i])
+//     }
+// }
+
+// console.log(somaNotas)
+// console.log(aprovados)
+
+// Versão orientada a objetos ↓
+
+class Aluno {
+    constructor (nome,notas) {
+        this._nome = nome;
+        this._notas = notas;
+    }
+    estaAprovado() {
+        return this._notas >= 7;
     }
 }
 
-console.log(somaNotas)
-console.log(aprovados)
+class Turma {
+    constructor(listaAlunos = []) {
+        this.alunos = listaAlunos
+    }
+
+    gerarRelatorioAprovados() {
+        return this.alunos.filter(aluno => aluno.estaAprovado())
+    }
+
+    gerarRelatorioReprovados() {
+        return this.alunos.filter(aluno => !aluno.estaAprovado())
+    }
+
+    adicionarAluno(aluno){
+        if (typeof aluno.estaAprovado === "function") {
+        this.alunos.push(aluno)
+        } else {
+            throw new Error("Erro ao ler aluno")
+        }
+    }
+
+    adicionarAlunos(alunos) {
+        alunos.forEach(element => {
+            this.adicionarAluno(element)
+        });
+    }
+}
+
+
+let alunos = [
+    new Aluno("Maria", 9),
+    new Aluno("João", 2),
+    new Aluno("Ana", 10),
+    new Aluno("Pedro", 7),
+    new Aluno("Lucas", 4),
+    new Aluno("Carla", 9),
+    new Aluno("Fernanda", 3),
+    new Aluno("Rafael", 7),
+    new Aluno("Juliana", 10),
+    new Aluno("Bruno", 5)
+];
+
+// Teste de validacao de aluno ↓
+// let aluno = "John"
+// matematica.adicionarAluno(aluno)
+
+
+const matematica = new Turma()
+matematica.adicionarAlunos(alunos)
+
+
+console.log(matematica.gerarRelatorioReprovados())
