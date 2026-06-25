891efe17b3cf080196504d961d1c509c76ae8ab8
Vitor <v.e.silva3007@gmail.com>
2026-06-25 10:02:05 -0300

    feat: adiçao de botoes para pausa e um indicador de maior valor

--- React Native/The_Reactor/App.js
+++ React Native/The_Reactor/App.js
@@ -1,21 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
 import { useEffect, useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 import {Accelerometer} from 'expo-sensors';
 
 export default function App() {
   const [leitura, setLeitura] = useState({x: 0, y: 0, z: 0});
+  const [pausa, setPausa] = useState(false);
+  const [maior, setMaior] = useState("")
 
   useEffect(() => {
+    //Checa se está pausado
+    if (pausa) return;
     //Define quantos milisegundos por leitura
     //100ms = 10 leituras por segundo
     Accelerometer.setUpdateInterval(50);
     //Inscreve: o sensor chama setLeitura toda vez que o valor muda 
     const inscricao = Accelerometer.addListener(setLeitura);
+    // Checagem do maior valor
+    if (Math.abs(leitura.x) > Math.abs(leitura.y) && Math.abs(leitura.x) > Math.abs(leitura.z)) {
+      setMaior("Eixo X")
+    } else if (Math.abs(leitura.y) > Math.abs(leitura.x) && Math.abs(leitura.y) > Math.abs(leitura.z)) {
+      setMaior("Eixo Y")
+    } else if (Math.abs(leitura.z) > Math.abs(leitura.y) && Math.abs(leitura.z) > Math.abs(leitura.x)) {
+      setMaior("Eixo Z")
+    }
     //cleanup: cancela inscrição quando o componente sai da tela
     //sem isso o componente continua rodando mesmo invisivel
     return () => inscricao.remove();
-  }, [])
+  }, [pausa, maior, leitura])
 
   return (
     <View style={styles.container}>
@@ -23,6 +35,9 @@ export default function App() {
       <Text style={styles.EixoX}>Eixo x: {leitura.x.toFixed(2)}</Text>
       <Text style={styles.EixoY}>Eixo y: {leitura.y.toFixed(2)}</Text>
       <Text style={styles.EixoZ}>Eixo z: {leitura.z.toFixed(2)}</Text>
+      <Button title="Parar" onPress={() => setPausa(true)}></Button>
+      <Button title="Retomar" onPress={() => setPausa(false)}></Button>
+      <Text style={styles.EixoY}>Maior valor: {maior}</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -51,6 +66,15 @@ const styles = StyleSheet.create({
   },
   EixoZ: {
     fontSize: 20, 
-    color: '#0dff00'
+    color: '#0dff00',
+    paddingBottom: 10
+  },
+  Botao: {
+        backgroundColor: '#4CAF50',
+      paddingVertical: 12,
+      paddingHorizontal: 25,
+    borderRadius: 8,
+    alignItems: 'center',
+    marginVertical: 5
   }
 });
